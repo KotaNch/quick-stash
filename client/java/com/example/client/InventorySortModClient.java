0cc1c84556fd1abcff5a6cff21407e1531825ed6
KotaNch <ilacihancov01@gmail.com>
2026-06-28 19:06:14 +0500

    feat: logging button pressing

---
+++ client/java/com/example/client/InventorySortModClient.java
@@ -0,0 +1,33 @@
+package com.example.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
+import org.lwjgl.glfw.GLFW;
+
+public class InventorySortModClient implements ClientModInitializer {
+
+
+	public static final KeyBinding.Category CATEGORY = KeyBinding.Category.create(Identifier.of("inventory-sort-mod", "main"));
+	public static KeyBinding sortKeyBinding;
+
+	@Override
+	public void onInitializeClient() {
+		sortKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.inventory-sort-mod.sort",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_G,
+				CATEGORY
+		));
+		ClientTickEvents.END_CLIENT_TICK.register(client ->{
+			while (sortKeyBinding.wasPressed()) {
+
+				System.out.println("Sort key pressed");
+			}
+				}
+		);
+	}
+}
