9b73156a0c7d10d63c080d6e7f891c0e242c8954
KotaNch <ilacihancov01@gmail.com>
2026-06-29 15:46:18 +0500

    Rename mod id from template-mod to inventory-sort-mod

--- src/main/java/com/example/TemplateMod.java
+++ src/main/java/com/example/TemplateMod.java
@@ -13,7 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TemplateMod implements ModInitializer {
-	public static final String MOD_ID = "template-mod";
+	public static final String MOD_ID = "inventory-sort-mod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
